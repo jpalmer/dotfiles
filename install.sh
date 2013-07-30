9e1a7b32a7722296011c0db38b3b14f0179c4864
John Palmer <jpal8929@physics.usyd.edu.au>
2013-07-30 15:26:16 -0600

    support laptop and slight improvement to install script to automatically restart xmonad

--- install.sh
+++ install.sh
@@ -9,10 +9,4 @@ do
     rm ~/"$x"
     ln -sf "$mydir"/"$x" ~/"$x"
 done
-#rm ~/.bashrc ~/.vimrc
-#rm -rf ~/.vim
-#now use soft links to use the files from the repo
-#ln -sf dotfiles/.bashrc Xdefaults .Xdefaults
-#ln -sf dotfiles/.bashrc ~/.bashrc
-#ln -sf dotfiles/.vimrc ~/.vimrc
-#ln -sf dotfiles/.vim ~/.vim
+xmonad --recompile && xmonad --restart
